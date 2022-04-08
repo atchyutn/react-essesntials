3e4d82b319d98b7fa99c50f2d96f1d3ab4e9f4a2
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2022-04-08 14:41:10 +0530

    Added multiple components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from "./App.js"
 
 ReactDOM.render(
-  <ul>
-    <li>Monday</li>
-    <li>Tuesday</li>
-    <li>Wednesday</li>
-    <li>Thursday</li>
-  </ul>,
+  <App />,
   document.getElementById('root')
 );
